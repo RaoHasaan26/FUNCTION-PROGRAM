34fd0311f0c1986b4fcfabf9a20b0bf8dccd51dd
RaoHasaan26 <162978737+RaoHasaan26@users.noreply.github.com>
2024-04-26 03:41:17 -0700

    Add files via upload
     Excited to share my latest accomplishment: completing a function program assignment as part of a challenging project! 💻
    
    🔍 In this assignment, I delved into TypeScript and JavaScript to create robust and efficient functions that solve real-world problems. From basic algorithms to complex logic, I crafted solutions that showcase my proficiency in these versatile programming languages.
    
    🔧 Leveraging TypeScript's static typing and JavaScript's dynamic capabilities, I ensured code reliability and flexibility, paving the way for scalable and maintainable applications.
    
    🎯 Whether it's handling data structures, implementing algorithms, or optimizing performance, I thrive on the intricacies of programming challenges and deliver results that exceed expectations.
    
    💼 This assignment not only honed my technical skills but also fostered collaboration and problem-solving in a team environment. I'm eager to apply these learnings to future endeavors and tackle even more ambitious projects.
    
    💬 Let's connect and discuss how my expertise in TypeScript and JavaScript can bring value to your team or project. I'm always up for new challenges and opportunities!

---
+++ app.js
@@ -0,0 +1,18 @@
+function greet() {
+    return "Hello World";
+}
+var message = greet();
+console.log(message);
+function sum() {
+    return "2 + 2";
+}
+var result = sum();
+console.log(result);
+
+
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+var result = sum(8, 5);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,22 @@
+function greet() {
+    return "Hello World";
+}
+let message: string = greet();
+console.log(message);
+
+
+
+
+function sum() {
+    return "2 + 2";
+}
+let result = sum();
+console.log(result);
+
+
+
+function sum(num1:number,num2:number) {
+    return num1 + num2;
+}
+let result = sum(8,5);
+console.log(result);
